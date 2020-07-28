62432bbe3d473b2fb0ccc82ff8498fd53aac5235
cesar4rroyo <cesardavid_09@hotmail.com>
2020-07-28 16:17:34 -0500

    animations

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -5,6 +5,7 @@ import { FlatList, ScrollView } from "react-native-gesture-handler";
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 const mapStateToProps = state => {
     return {
@@ -60,26 +61,30 @@ class AboutUs extends Component {
         else if (this.props.leaders.errMess) {
             return (
                 <ScrollView>
-                    <History />
-                    <Card
-                        title='Corporate Leadership'>
-                        <Text>{this.props.leaders.errMess}</Text>
-                    </Card>
+                    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                        <History />
+                        <Card
+                            title='Corporate Leadership'>
+                            <Text>{this.props.leaders.errMess}</Text>
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
         else {
             return (
                 <ScrollView>
-                    <History />
-                    <Card
-                        title='Corporate Leadership'>
-                        <FlatList
-                            data={this.props.leaders.leaders}
-                            renderItem={renderLeader}
-                            keyExtractor={item => item.id.toString()}
-                        />
-                    </Card>
+                    <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                        <History />
+                        <Card
+                            title='Corporate Leadership'>
+                            <FlatList
+                                data={this.props.leaders.leaders}
+                                renderItem={renderLeader}
+                                keyExtractor={item => item.id.toString()}
+                            />
+                        </Card>
+                    </Animatable.View>
                 </ScrollView>
             );
         }
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Text } from "react-native";
 import { Card } from "react-native-elements";
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
 
@@ -15,19 +16,21 @@ class Contact extends Component {
 
     render() {
         return (
-            <Card
-                title="Contact Information"
-            >
-                <Text
-                    style={{ margin: 10 }}>
-                    121, Clear Water Bay Road <br />
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                <Card
+                    title="Contact Information"
+                >
+                    <Text
+                        style={{ margin: 10 }}>
+                        121, Clear Water Bay Road <br />
                     Clear Water Bay, Kowloon <br />
                     HONG KONG <br />
                     Tel: +852 1234 5678 <br />
                     Fax: +852 8765 4321 <br />
                     Email:confusion@food.net
                 </Text>
-            </Card>
+                </Card>
+            </Animatable.View>
         )
     }
 
