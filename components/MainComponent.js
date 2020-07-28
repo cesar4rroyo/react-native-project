8e54c351feb900fbf38e792ca8fb3082fdfa21f2
cesar4rroyo <cesardavid_09@hotmail.com>
2020-07-28 15:47:16 -0500

    swipe option and alerts

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -11,6 +11,7 @@ import { Icon } from "react-native-elements"
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
+import Favorites from './FavoriteComponent';
 
 
 const mapStateToProps = state => {
@@ -119,6 +120,23 @@ const ReservationNavigator = createStackNavigator({
     })
 })
 
+const FavoritesNavigator = createStackNavigator({
+    Favorites: { screen: Favorites }
+}, {
+    navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTitleStyle: {
+            color: "#fff"
+        },
+        headerTintColor: "#fff",
+        headerLeft: <Icon name="menu" size={24}
+            iconStyle={{ color: 'white' }}
+            onPress={() => navigation.navigate('DrawerToggle')} />
+    })
+})
+
 
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
@@ -192,6 +210,22 @@ const MainNavigator = createDrawerNavigator({
             drawerIcon: ({ tintColor, focused }) => (
                 <Icon
                     name='cutlery'
+                    type='font-awesome'
+                    size={24}
+                    iconStyle={{ color: tintColor }}
+                />
+            ),
+        }
+    },
+    Favorites:
+    {
+        screen: FavoritesNavigator,
+        navigationOptions: {
+            title: 'My Favorites',
+            drawerLabel: 'My Favorites',
+            drawerIcon: ({ tintColor, focused }) => (
+                <Icon
+                    name='heart'
                     type='font-awesome'
                     size={24}
                     iconStyle={{ color: tintColor }}
--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -173,3 +173,8 @@ export const addFavorite = (dishId) => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: dishId
 });
+
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+});  
