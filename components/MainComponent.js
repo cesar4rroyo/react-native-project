8cd898ef78a41497a4354aa976773077099c60b2
cesar4rroyo <cesardavid_09@hotmail.com>
2020-07-29 18:22:06 -0500

    local notifications and secure store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -12,6 +12,7 @@ import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoriteComponent';
+import Login from './LoginComponent';
 
 
 const mapStateToProps = state => {
@@ -137,6 +138,23 @@ const FavoritesNavigator = createStackNavigator({
     })
 })
 
+const LoginNavigator = createStackNavigator({
+    Login: { screen: Login }
+}, {
+    navigationOptions: ({ navigation }) => ({
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTitleStyle: {
+            color: "#fff"
+        },
+        headerTintColor: "#fff",
+        headerLeft: <Icon name="menu" size={24}
+            iconStyle={{ color: 'white' }}
+            onPress={() => navigation.toggleDrawer()} />
+    })
+});
+
 
 const CustomDrawerContentComponent = (props) => (
     <ScrollView>
@@ -157,6 +175,22 @@ const CustomDrawerContentComponent = (props) => (
 
 
 const MainNavigator = createDrawerNavigator({
+    Login:
+    {
+        screen: LoginNavigator,
+        navigationOptions: {
+            title: 'Login',
+            drawerLabel: 'Login',
+            drawerIcon: ({ tintColor, focused }) => (
+                <Icon
+                    name='sign-in'
+                    type='font-awesome'
+                    size={24}
+                    iconStyle={{ color: tintColor }}
+                />
+            ),
+        }
+    },
     Home:
     {
         screen: HomeNavigator,
@@ -232,9 +266,10 @@ const MainNavigator = createDrawerNavigator({
                 />
             ),
         }
-    }
+    },
 
 }, {
+    initialRouteName: 'Home',
     drawerBackgroundColor: '#D1C4E9',
     contentComponent: CustomDrawerContentComponent
 });
--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,8 @@ import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert } fro
 import { Card } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker'
 import * as Animatable from 'react-native-animatable';
+import { Permissions, Notifications } from 'expo';
+
 
 class Reservation extends Component {
 
@@ -22,7 +24,31 @@ class Reservation extends Component {
     };
 
 
-
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for ' + date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
 
     handleReservation() {
         Alert.alert(
@@ -32,7 +58,7 @@ class Reservation extends Component {
             'Date and Time: ' + this.state.date,
             [
                 { text: 'Cancel', onPress: () => { console.log('Cancel Pressed'); this.resetForm() }, style: 'cancel' },
-                { text: 'OK', onPress: () => { this.resetForm() } },
+                { text: 'OK', onPress: () => { this.presentLocalNotification(this.state.date); this.resetForm() } },
             ],
             { cancelable: false }
         );
